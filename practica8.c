631c922c94b16aa67e6de518215cb99c53212468
Adolfo Roman <adolfo.roman2@gmail.com>
2021-01-16 20:27:43 -0600

    Variables y estructura inicial

--- practica8.c
+++ practica8.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+/*
+En esta practica escogi hacer un programa que originalmente iba a
+hacer para la practica 7, pero mejor lo cambiare para esta ya que me
+gustaria probar con switch
+*/
+
 int main (void) {
 
+  //Declarar variables
+  float c, f, k;
+  int a, b;
+
+  //Estructura inicial
+  printf("***Convertidor de Temperatura***\n");
+  printf("\n");
+  printf("Escoge la temperatura inicial:\n");
+  printf("\n");
+  printf("1) Celsius\n");
+  printf("2) Fahrenheit\n");
+  printf("3) Kelvin\n");
+  scanf("%i\n",a);
 
 }
